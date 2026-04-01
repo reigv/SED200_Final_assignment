0d3075b8009bff3291e2ccbfb64f84373a4f7c13
rhht <reig.vaeo.soiv099@gmail.com>
2026-04-01 13:11:28 -0400

    1.8

--- library.cpp
+++ library.cpp
@@ -12,10 +12,20 @@ Person::Person(std::string n, std::string d, Gender g)
 
 Person::~Person() {}
 
+std::ostream& operator<<(std::ostream& os, const Gender& g) {
+    if (g==male) { return os << "Male"; }
+    else if (g==female) { return os << "Female"; }
+    else { return os << "Other"; }
+}
+
 void Person::display() const {
     std::cout << "Name: " << full_name << std::endl;
     std::cout << "Date of Birth: " << date_birth << std::endl;
     std::cout << "Gender: " << gender << std::endl;
+}
+
+std::string Person::get_name() const {
+    return full_name;
 }
 
 
--- library.h
+++ library.h
@@ -8,7 +8,7 @@
 const double COURSE_SIZE = 6*6; // 6 courses, 6 students per course
 const enum Gender { male, female, other };
 
-// abstract Person class
+// abstract Person class-----------------------------------------
 class Person {
 protected:
     std::string full_name;
@@ -22,9 +22,19 @@ public:
     // pure destructor
     virtual ~Person() = 0;  // abstract class, virtual helps for derived classes
 
+    // since gender (enum) -- overload the ostream << 
+    friend std::ostream& operator<<(std::ostream& os, const Gender& g);
     virtual void display() const; // virtual helps for derived classes to override
 
     virtual std::string get_name() const =0; // pure virtual function, must be implemented by derived classes
 };
+
+//-------------------------------------------------------------
+// Student class derived from Person--------------------------------
+class Student : public Person {
+private:
+    double gpa;
+};
+
 
 #endif // LIBRARY_H_
