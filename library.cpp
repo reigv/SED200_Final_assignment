cef3dd50e7ae97ef55d10c2831b60ef3782857f1
rhht <reig.vaeo.soiv099@gmail.com>
2026-04-01 11:53:25 -0400

    1.2

---
+++ library.cpp
@@ -0,0 +1,21 @@
+// library.cpp -- implementation file for the library of classes
+
+#include "library.h"
+#include <iostream>
+
+// -------------------------------------
+Person::Person() 
+: full_name(""), date_birth(""), gender(other) {}
+
+Person::Person(std::string n, std::string d, Gender g)
+: full_name(n), date_birth(d), gender(g) {}
+
+Person::~Person() {}
+
+void Person::display() const {
+    std::cout << "Name: " << full_name << std::endl;
+    std::cout << "Date of Birth: " << date_birth << std::endl;
+    std::cout << "Gender: " << gender << std::endl;
+}
+
+
---
+++ library.h
@@ -0,0 +1,30 @@
+// library.h -- header file for the library of classes
+
+#ifndef LIBRARY_H_
+#define LIBRARY_H_
+
+#include <string>
+
+const double COURSE_SIZE = 6*6; // 6 courses, 6 students per course
+const enum Gender { male, female, other };
+
+// abstract Person class
+class Person {
+protected:
+    std::string full_name;
+    std::string date_birth;
+    Gender gender;
+
+public:
+    Person();
+    Person(std::string n, std::string d, Gender g);
+
+    // pure destructor
+    virtual ~Person() = 0;  // abstract class, virtual helps for derived classes
+
+    virtual void display() const; // virtual helps for derived classes to override
+
+    virtual std::string get_name() const =0; // pure virtual function, must be implemented by derived classes
+};
+
+#endif // LIBRARY_H_
